da3d46ba9be29688e0f39db47021da1056e2f270
ademola <adeadewole1712@gmail.com>
2022-10-03 16:49:47 +0100

    use navigate, athenticating the user and logging out

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React from "react";
-import { Routes, Route, Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import AddPost from "./pages/AddPost";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import { signOut } from "firebase/auth";
+
+import '../src/App.css'
+import { auth } from "./Firebase-config";
 
 function App() {
+
+  const [isAuth, setIsAuth] = useState(false);
+
+
+  const signUserOut = () => {
+    signOut(auth).then(response => {
+      localStorage.clear();
+      setIsAuth(false)
+      window.location.pathname = '/login';
+    })
+  }
+
   return (
+    <>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/add-post'>Create Post</Link>
+        {!isAuth ? <Link to='/login'>Login</Link> : <button onClick={signUserOut}>Log Out</button>}
+      </nav>
 
-    <Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-post" element={<AddPost />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+      </Routes>
 
-      <Route path="/" element={<Home />} />
-      <Route path="/add-post" element={<AddPost />} />
-      <Route path="/login" element={<Login />} />
+    </>
 
-    </Routes>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import {auth, provider} from '../Firebase-config'
+import {signInWithPopup} from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
-const Login = () => {
+const Login = ({setIsAuth, isAuth}) => {
+
+  const navigate = useNavigate()
+
+  const signInWithGoogle = () =>{
+    signInWithPopup(auth, provider).then(response => {
+      localStorage.setItem('isAuth', true)
+      setIsAuth(true)
+      console.log(response)
+      navigate('/')
+    })
+  }
+
+
+
   return (
-    <div>Login</div>
+    <div className='login-page'>
+      <p>Sign in with Google to continue</p>
+      <button className='login-with-google-btn' onClick={signInWithGoogle} >Sign in with Google</button>
+    </div>
   )
 }
 
-export default Login
+export default Login 
